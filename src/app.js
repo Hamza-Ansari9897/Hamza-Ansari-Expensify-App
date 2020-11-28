34be39d2733ff5e3eb1cb76b8095054a8f5a2a0d
Hamza Ansari <ha9897534395@gmail.com>
2020-11-29 01:06:13 +0530

    setup

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore'
 import {addExpense} from './actions/expenses';
@@ -13,22 +12,7 @@ import 'react-dates/lib/css/_datepicker.css'
 
 const store=configureStore();
 
-// // store.dispatch(addExpense({description:'Water Bill',amount:100}))
 
-// // store.dispatch(addExpense({description:'Gas Bill',amount:700,createAt:100}))
-
-// // store.dispatch(addExpense({description:'Electric Bill',amount:300}))
-
-// // store.dispatch(addExpense({description:'Rent ',amount:6000,createAt:1000}))
-
-// // store.dispatch(addExpense({description:'School fees',amount:500,createAt:300}))
-
-
-
-// const state=store.getState();
-// const visibleExpenses=getVisibleExpenses(state.expenses,state.filters);
-// console.log(visibleExpenses);
-console.log("test")
 const jsx=(
     <Provider store={store}>
        <AppRouter/>
